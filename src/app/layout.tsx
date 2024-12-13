e00654e9efe2f4edbecd7e71eb35a00b93d50e43
arrinal <arrinalsf@gmail.com>
2024-12-13 22:56:52 +0700

    fix favicon mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,32 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Bakmie Darmono",
   description: "Bakmie karet otentik dengan topping paling lengkap di Jakarta",
+  manifest: '/favicon/site.webmanifest',
   icons: {
-    icon: {
-      url: '/favicon/favicon.ico',
-      sizes: 'any',
-    },
-    apple: {
-      url: '/favicon/apple-touch-icon.png',
-      sizes: '180x180',
-      type: 'image/png',
-    },
+    icon: '/favicon/favicon.ico',
+    shortcut: '/favicon/favicon.ico',
+    other: [
+      {
+        url: '/favicon/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      }
+    ]
   },
   openGraph: {
     type: 'website',
